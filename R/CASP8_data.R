e8661981d2074eb3f5ec601b037ebe9f72fff413
arunangshu-github <joyman8100@gmail.com>
2021-03-23 22:31:25 -0500

    Data set and it's documentation

---
+++ R/CASP8_data.R
@@ -0,0 +1,15 @@
+#' Title
+#'
+#' Description
+#'
+#'
+#' @format A data frame with 144 rows and 10 variables:
+#' \describe {
+#'     \item{cline}{}
+#'     \item{Exp}{}
+#'     \item{dose}{}
+#'     \item{ncells}{}
+#'     \item{ncolonies}{}
+#' }
+#' @source \url{}
+"CASP8_data"
